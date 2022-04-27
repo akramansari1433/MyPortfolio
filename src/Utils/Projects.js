c725dfd17ac105700bd521ffd7298862cd7c858e
akramansari1433 <70878208+akramansari1433@users.noreply.github.com>
2022-04-27 10:29:47 +0530

    projects added

--- src/Utils/Projects.js
+++ src/Utils/Projects.js
@@ -1,18 +1,40 @@
 import roboFriend from "./img/RoboFriends.png";
 import smartBrain from "./img/SmartBrain.png";
-import bgGenerator from "./img/bgGenerator.png";
 import covidTracker from "./img/covidTracker.png";
 import facebookMessengerClone from "./img/facebookMessengerClone.png";
+import socialMedia from "./img/socialMedia.png";
+import portfolioSpectrum from "./img/portfolioSpectrum.png";
 
 export const project_list = [
    {
-      image: roboFriend,
-      title: "RoboFriends",
-      des: "Fetching robots online through API and searching robots",
-      tags: ["HTML", "CSS", "React", "JavaScript"],
+      image: portfolioSpectrum,
+      title: "Portfolio Website",
+      des: "Portfolio Website for PhotoGrapher.",
+      tags: ["React", "Node", "Express", "Firebase"],
       links: {
-         liveView: "https://akramansari1433.github.io/RoboFriends/",
-         github: "https://github.com/akramansari1433/RoboFriends.git",
+         liveView: "https://spectrumphotography.in/",
+         github: "https://github.com/akramansari1433/spectrum-client.git",
+      },
+   },
+   {
+      image: socialMedia,
+      title: "Social Media App",
+      des: "Complete Socila Media App with Chatting feature.",
+      tags: ["React", "Redux", "Node", "Express", "Firebase"],
+      links: {
+         liveView: "https://social-media-a38ca.web.app/login",
+         github: "https://github.com/akramansari1433/social-media-client.git",
+      },
+   },
+   {
+      image: facebookMessengerClone,
+      title: "Facebook Messenger Clone",
+      des: "Chat in realtime like Facebook Messenger",
+      tags: ["HTML", "CSS", "JavaScript", "FireBase"],
+      links: {
+         liveView: "https://facebook-messenger-clone-8c9fa.web.app/",
+         github:
+            "https://github.com/akramansari1433/facebook-messenger-clone.git",
       },
    },
    {
@@ -26,15 +48,16 @@ export const project_list = [
       },
    },
    {
-      image: bgGenerator,
-      title: "Background Generator",
-      des: "Background Generator using Linear Gradient",
-      tags: ["HTML", "CSS", "JavaScript"],
+      image: roboFriend,
+      title: "RoboFriends",
+      des: "Fetching robots online through API and searching robots",
+      tags: ["HTML", "CSS", "React", "JavaScript"],
       links: {
-         liveView: "https://akramansari1433.github.io/background-generator/",
-         github: "https://github.com/akramansari1433/background-generator.git",
+         liveView: "https://akramansari1433.github.io/RoboFriends/",
+         github: "https://github.com/akramansari1433/RoboFriends.git",
       },
    },
+
    {
       image: covidTracker,
       title: "Live Covid Tracker",
@@ -43,17 +66,6 @@ export const project_list = [
       links: {
          liveView: "https://akramansari1433.github.io/covid-tracker/",
          github: "https://github.com/akramansari1433/covid-tracker.git",
-      },
-   },
-   {
-      image: facebookMessengerClone,
-      title: "Facebook Messenger Clone",
-      des: "Chat in realtime like Facebook Messenger",
-      tags: ["HTML", "CSS", "JavaScript", "FireBase"],
-      links: {
-         liveView: "https://facebook-messenger-clone-8c9fa.web.app/",
-         github:
-            "https://github.com/akramansari1433/facebook-messenger-clone.git",
       },
    },
 ];
